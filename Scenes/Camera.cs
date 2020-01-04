c38231944d68bd76146f1a51aec710d50aab9428
Olle Astré <olle.astre@tutamail.com>
2020-01-04 20:07:57 +0100

    Implement directional billboards for enemies & fix weapon and head sway

---
+++ Scenes/Camera.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+public class Camera : Godot.Camera
+{
+    float swayAmount = 0.7f;
+    float smoothAmount = 10f;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        float sidewaysMovement = 0;
+
+        if (Input.IsActionPressed("move_left"))
+            sidewaysMovement = 1;
+        if (Input.IsActionPressed("move_right"))
+            sidewaysMovement = -1; 
+
+        sidewaysMovement *= swayAmount;
+
+        float rotation = (sidewaysMovement/1) - RotationDegrees.z * smoothAmount * delta;
+
+        Vector3 finalRotation= new Vector3(0, 0, rotation);
+
+        // Rotate
+        SetRotationDegrees(RotationDegrees + finalRotation);
+    }
+}
--- Scripts/WeaponSway.cs
+++ Scripts/WeaponSway.cs
@@ -3,41 +3,38 @@ using System;
 
 public class WeaponSway : Spatial
 {
-    [Export] private float amount = 10;
-    [Export] private float maxAmount = 100;
-    [Export] private float smoothAmount = 1;  
+    [Export] private float amount = 0.5f;
+    [Export] private float smoothAmount = 10f;  
 
     private  Vector3 initialPosition;
 
-    float movementX;
-    float movementY;
+    float mouseMovementX;
+    float mouseMovementY;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        //initialPosition = Transform.origin;
+        initialPosition = Transform.origin;
     }
     
     public override void _Input(InputEvent @event)
     {
         if (@event is InputEventMouseMotion)
         {
-            float movementX = ((InputEventMouseMotion)@event).Relative.x * amount;
-            float movementY = ((InputEventMouseMotion)@event).Relative.y * amount;
-            
+            mouseMovementX = ((InputEventMouseMotion)@event).Relative.x * amount;
+            mouseMovementY = ((InputEventMouseMotion)@event).Relative.y * amount;
         }
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
-        initialPosition = Translation;
-        movementX = Mathf.Clamp(movementX, -maxAmount, maxAmount);
-        movementY = Mathf.Clamp(movementY, -maxAmount, maxAmount);
-        Vector3 finalposition = new Vector3(movementX, movementY, 0);
-        Console.WriteLine(movementX);
-        Console.WriteLine(movementY);
-        //SetTranslation(Vector3.Lerp(Transform.origin, finalposition + initialPosition, delta * smoothAmount));
-        Translation.LinearInterpolate(finalposition + initialPosition, delta * smoothAmount);
+        float movementX = (mouseMovementY/5) - RotationDegrees.x * smoothAmount * delta;
+        float movementY = (mouseMovementX/5) - RotationDegrees.y * smoothAmount * delta;
+
+        Vector3 finalMovement = new Vector3(movementX, movementY, 0);
+
+        // Rotate
+        SetRotationDegrees(RotationDegrees + finalMovement);
     }
 }
