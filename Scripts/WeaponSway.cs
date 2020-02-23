3fecb0c4b654670932956d81fe6eb650e8996d6f
Olle Astré <olle.astre@tutamail.com>
2020-02-23 13:14:53 +0100

    Clamp weapon sway

--- Scripts/WeaponSway.cs
+++ Scripts/WeaponSway.cs
@@ -5,6 +5,7 @@ public class WeaponSway : Spatial
 {
     [Export] private float amount = 0.5f;
     [Export] private float smoothAmount = 10f;  
+    [Export] private float maxSway = 15f; 
 
     private  Vector3 initialPosition;
 
@@ -32,9 +33,16 @@ public class WeaponSway : Spatial
         float movementX = (mouseMovementY/5) - RotationDegrees.x * smoothAmount * delta;
         float movementY = (mouseMovementX/5) - RotationDegrees.y * smoothAmount * delta;
 
-        Vector3 finalMovement = new Vector3(movementX, movementY, 0);
+        Vector3 movement = new Vector3(movementX, movementY, 0);
+        Vector3 finalMovement = RotationDegrees + movement;
+
+        // Limit final movement
+        finalMovement.x = Mathf.Clamp(finalMovement.x, -maxSway, maxSway);
+        finalMovement.y = Mathf.Clamp(finalMovement.y, -maxSway, maxSway);
+
+        
 
         // Rotate
-        SetRotationDegrees(RotationDegrees + finalMovement);
+        SetRotationDegrees(finalMovement);
     }
 }
